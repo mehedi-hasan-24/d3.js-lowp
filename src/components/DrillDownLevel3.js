92f5bcedefbef438cd3638fff1f0abba93b07e25
mehedi-lowp <mehedi@lowp.jp>
2021-06-08 12:01:30 +0600

    data added to the table

--- src/components/DrillDownLevel3.js
+++ src/components/DrillDownLevel3.js
@@ -1,5 +1,7 @@
-import { select } from "d3-selection";
+import { scaleLinear } from "d3-scale";
+import * as d3 from "d3";
 import React, { useEffect, useRef } from "react";
+import useResizeObserver from "../utilities/useResizeObserver";
 
 const graphData = [
   {
@@ -105,18 +107,69 @@ const graphData = [
 ];
 
 const DrillDownLevel3 = () => {
-  const svgRef = useRef();
+  const tableRef = useRef();
   const wrapperRef = useRef();
-  // useEffect(() => {
-  //   svgRef.current.innerHTML = "";
-  //   const svg = select(svgRef.current);
+  const dimensions = useResizeObserver(wrapperRef);
 
-  //   svg
-  //     .attr("height", 10)
-  //     .attr("width", 300)
-  //     .style("border", "1px solid black")
-  //     .style("background-color", "blue");
-  // }, [svgRef.current]);
+  useEffect(() => {
+    tableRef.current.innerHTML = "";
+
+    dimensions.boundedHeight =
+      dimensions.height - dimensions.margins.top - dimensions.margins.bottom;
+    dimensions.boundedWidth =
+      dimensions.width - dimensions.margins.left - dimensions.margins.right;
+
+    let tableDiv = d3.select(tableRef.current);
+
+    tableDiv
+      .style("height", dimensions.boundedHeight + "px")
+      .style("width", dimensions.boundedWidth + "px")
+      .style("border", "1px solid black")
+      .style("overflow-y", "scroll");
+
+    let table = tableDiv.append("table").attr("width", "100%");
+    let thead = table.append("thead");
+    let tbody = table.append("tbody");
+
+    const columnName = [
+      "店舗名称",
+      "エリア",
+      "ロケーション",
+      ...graphData[0].areas[0].locations[0].locationPeriods,
+    ];
+    thead
+      .append("tr")
+      .selectAll("th")
+      .data(columnName)
+      .enter()
+      .append("th")
+      .style("width", (_, i) => (i < 3 ? "10%" : ""))
+      .text((column) => column);
+
+    for (let i = 0; i < graphData.length; i++) {
+      for (let j = 0; j < graphData[i].areas.length; j++) {
+        for (let k = 0; k < graphData[i].areas[j].locations.length; k++) {
+          let tr = tbody.append("tr");
+          tr.append("td")
+            .text(graphData[i].shopName)
+            .style("text-align", "center");
+          tr.append("td")
+            .text(graphData[i].areas[j].areaCode)
+            .style("text-align", "center");
+          tr.append("td")
+            .text(graphData[i].areas[j].locations[k].locationCode)
+            .style("text-align", "center");
+          graphData[i].areas[j].locations[k].locationData.forEach((d) => {
+            tr.append("td").text(d).style("text-align", "left");
+          });
+        }
+      }
+    }
+
+    console.log(dimensions.boundedWidth);
+
+    // console.log(table);
+  }, [dimensions, wrapperRef.current]);
 
   const getSvg = (width) => {
     return (
@@ -125,51 +178,71 @@ const DrillDownLevel3 = () => {
       ></svg>
     );
   };
-  console.log("graphData =>>>", graphData);
-  const drawTable = () => {
-    for (let i = 0; i < graphData.length; i++) {
-      for (let j = 0; j < graphData[i].areas.length; j++) {
-        graphData[i].areas[j].locations.map((loc, locIndex) => {
-          // console.log(loc.locationCode);
-          return <div>{loc.locationCode}</div>;
-        });
-      }
-    }
-  };
 
   return (
-    <div>
-      <table
+    <div ref={wrapperRef}>
+      <div ref={tableRef}></div>
+      {/* <table
         style={{
           width: "100%",
-          borderBottom: "1px solid black",
+          borderCollapse: "collapse",
         }}
       >
-        <tr
-          style={{
-            textAlign: "center",
-          }}
-        >
-          <th style={{ width: "10%" }}>店舗名称</th>
-          <th style={{ width: "10%" }}>エリア</th>
-          <th style={{ width: "10%" }}>ロケーション</th>
-          <th style={{ width: "35%" }}>2020/08</th>
-          <th style={{ width: "35%" }}>2021/02</th>
-        </tr>
-        {/* <tr>
-          <td style={{ width: "10%", textAlign: "center" }}>六本⽊ヒルズ</td>
-          <td style={{ width: "10%", textAlign: "center" }}>11</td>
-          <td style={{ width: "10%", textAlign: "center" }}>110</td>
-          <td style={{ width: "35%", textAlign: "left" }}>
-            {getSvg(100)} 123456
-          </td>
-          <td style={{ width: "35%", textAlign: "left" }}>
-            {getSvg(200)} 123456
-          </td>
-        </tr> */}
-      </table>
+        <thead>
+          <tr style={{}}>
+            <th style={{ width: "10%", borderBottom: "1px solid black" }}>
+              店舗名称
+            </th>
+            <th style={{ width: "10%", borderBottom: "1px solid black" }}>
+              エリア
+            </th>
+            <th style={{ width: "10%", borderBottom: "1px solid black" }}>
+              ロケーション
+            </th>
+            {graphData[0].areas[0].locations[0].locationPeriods.map(
+              (locPeriod) => (
+                <th
+                  style={{
+                    width: "35%",
+                    borderBottom: "1px solid black",
+                    borderLeft: "1px solid black",
+                  }}
+                  key={locPeriod}
+                >
+                  {locPeriod}
+                </th>
+              )
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {graphData.map((shop, shopIndex) =>
+            shop.areas.map((area, areaIndex) =>
+              area.locations.map((loc, locIndex) => (
+                <tr key={locIndex}>
+                  <td style={{ textAlign: "center" }}>{shop.shopName}</td>
+                  <td style={{ textAlign: "center" }}>{area.areaCode}</td>
+                  <td style={{ textAlign: "center" }}>{loc.locationCode}</td>
+                  {loc.locationData.map((locData, locDataIndex) => (
+                    <td
+                      style={{
+                        width: "35%",
+                        borderLeft: "1px solid black",
+                      }}
+                      key={locDataIndex}
+                      ref={tdRef}
+                    >
+                      {locData}
+                    </td>
+                  ))}
+                </tr>
+              ))
+            )
+          )}
+        </tbody>
 
-      {drawTable()}
+        <tfoot></tfoot>
+      </table> */}
     </div>
   );
 };
